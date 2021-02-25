e1677978d178781b400b82f5190b9c53e2829d5a
Tyler Grey Hilliard <tylerghilliard@gmail.com>
2021-02-25 16:48:26 -0600

    challenge 1

---
+++ Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    private float propellerSpeed = 2500.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * propellerSpeed);
+        
+    }
+}
